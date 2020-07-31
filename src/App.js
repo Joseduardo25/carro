8d2d46ced9d03a00b4f9ca649f5b9ceef25a4f7b
Eduardo Calachua <jocdwardo25@gmail.com>
2020-07-31 15:44:29 -0500

    explicacion

--- src/App.js
+++ src/App.js
@@ -1,52 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Component, useState } from 'react';
 
 function App() {
-  const motos = [
-    {
-      imagen:'https://flux.somosmoto.pe/r/https://somosmoto.pe/images/models/colors/wanxin-wx200gy-7-2019-blanco-f36380.png?width=1082',
-      color: 'morado',
-      precio: 123,
-      cilindrada: 1500,
-    },
-    {
-      imagen:'https://flux.somosmoto.pe/r/https://somosmoto.pe/images/models/colors/wanxin-wx200gy-7-2019-blanco-f36380.png?width=1082',
-      color: 'morado',
-      precio: 123,
-      cilindrada: 1500,
-    },
-    {
-      imagen:'https://flux.somosmoto.pe/r/https://somosmoto.pe/images/models/colors/wanxin-wx200gy-7-2019-blanco-f36380.png?width=1082',
-      color: 'morado',
-      precio: 123,
-      cilindrada: 1500,
-    }
-  ]
-  const[motitos,setMotitos] = useState(motos)
-  function onClickAdd(){
-    setMotitos(motitos)
-    
-    }
+  // Array de nombres iniciales
+  const array_nombres = ['Luis', 'Eduardo', 'Mateo']
+
+  // Estados de React
+  const [valueInput, setValueInput] = useState('')
+  const [nombres, setNombres] = useState(array_nombres)
+
+  // Función que detecta cuando escribo
+  function onChangeInput(event) {
+    // event.target.value es lo que escribo antes de setearlo en valueInput
+    setValueInput(event.target.value)
+  }
+
+  // Acción a ejecutarse cuando se da click al botón
+  function onClickEnviar() {
+    // Primero creo una copia de nombres
+    const copyNombres = [...nombres]
+    // Luego agrego a la copia el valor del input
+    copyNombres.push(valueInput)
+    /*
+      Ahora seteo el nuevo array de nombres con la copia
+      del antiguo array con el nuevo nombre
+    */ 
+    setNombres(copyNombres)
+    // Finalmente limpio el input donde se tipea
+    setValueInput('')
+  }
+
   return (
-     <div>
+    <div>
       <div>
-        {motos.map((e) => 
-        <div>
-          <div>
-            {e.precio}
-          </div>
-          <img style={{width:'200px', heigth:'200px'}} src={e.imagen}/>
-        </div> )}
-      </div>  
+        {nombres.map((name) => (
+          <div>Hola <strong>{name}</strong></div>
+        ))}
+      </div>
       <div>
-        <input></input>
-        <button onClick={onClickAdd}>Agregar</button>
+        <input value={valueInput} onChange={onChangeInput} />
+        <button onClick={onClickEnviar}>
+          Enviar
+        </button>
       </div>
     </div>
-
-  );
+  )
 }
 
 export default App;
