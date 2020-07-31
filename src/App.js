b28a8efe69fec344da8d175d6a3ff81e8d22f176
Eduardo Calachua <jocdwardo25@gmail.com>
2020-07-31 16:40:27 -0500

    segundo ejemplo

--- src/App.js
+++ src/App.js
@@ -3,47 +3,34 @@ import './App.css';
 import { Component, useState } from 'react';
 
 function App() {
-  // Array de nombres iniciales
-  const array_nombres = ['Luis', 'Eduardo', 'Mateo']
 
-  // Estados de React
-  const [valueInput, setValueInput] = useState('')
-  const [nombres, setNombres] = useState(array_nombres)
+  const array_nombres = ['juanita', 'pepita', 'maria']
+  const[valueInput, setValueInput] = useState('') 
+  const[nombres,setNombres] = useState(array_nombres) 
 
-  // Función que detecta cuando escribo
-  function onChangeInput(event) {
-    // event.target.value es lo que escribo antes de setearlo en valueInput
-    setValueInput(event.target.value)
+  function onChangeInput(e) {
+    setValueInput(e.target.value)
   }
 
-  // Acción a ejecutarse cuando se da click al botón
-  function onClickEnviar() {
-    // Primero creo una copia de nombres
+  function onClickEnviar(){
     const copyNombres = [...nombres]
-    // Luego agrego a la copia el valor del input
     copyNombres.push(valueInput)
-    /*
-      Ahora seteo el nuevo array de nombres con la copia
-      del antiguo array con el nuevo nombre
-    */ 
     setNombres(copyNombres)
-    // Finalmente limpio el input donde se tipea
     setValueInput('')
   }
-
   return (
     <div>
       <div>
-        {nombres.map((name) => (
-          <div>Hola <strong>{name}</strong></div>
-        ))}
+        {nombres.map((e) => (<div>{e}</div>))}
       </div>
-      <div>
-        <input value={valueInput} onChange={onChangeInput} />
-        <button onClick={onClickEnviar}>
+      <input 
+        value={valueInput} 
+        onChange={onChangeInput}/>
+      <button  
+        onClick={onClickEnviar}
+        >
           Enviar
-        </button>
-      </div>
+      </button>
     </div>
   )
 }
