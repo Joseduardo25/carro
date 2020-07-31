87d25b52e99b048ae73593dbcc1ddd9d28f9b179
Eduardo Calachua <jocdwardo25@gmail.com>
2020-07-31 14:46:33 -0500

    ejemplo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Component, useState } from 'react';
 
 function App() {
+  const motos = [
+    {
+      imagen:'https://flux.somosmoto.pe/r/https://somosmoto.pe/images/models/colors/wanxin-wx200gy-7-2019-blanco-f36380.png?width=1082',
+      color: 'morado',
+      precio: 123,
+      cilindrada: 1500,
+    },
+    {
+      imagen:'https://flux.somosmoto.pe/r/https://somosmoto.pe/images/models/colors/wanxin-wx200gy-7-2019-blanco-f36380.png?width=1082',
+      color: 'morado',
+      precio: 123,
+      cilindrada: 1500,
+    },
+    {
+      imagen:'https://flux.somosmoto.pe/r/https://somosmoto.pe/images/models/colors/wanxin-wx200gy-7-2019-blanco-f36380.png?width=1082',
+      color: 'morado',
+      precio: 123,
+      cilindrada: 1500,
+    }
+  ]
+  const[motitos,setMotitos] = useState(motos)
+  function onClickAdd(){
+    setMotitos(motitos)
+    
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+      <div>
+        {motos.map((e) => 
+        <div>
+          <div>
+            {e.precio}
+          </div>
+          <img style={{width:'200px', heigth:'200px'}} src={e.imagen}/>
+        </div> )}
+      </div>  
+      <div>
+        <input></input>
+        <button onClick={onClickAdd}>Agregar</button>
+      </div>
     </div>
+
   );
 }
 
