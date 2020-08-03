614c5031d81c64777b9f1c2bb8647867188519cf
Eduardo Calachua <jocdwardo25@gmail.com>
2020-08-03 17:24:43 -0500

    arrays_objetos

--- src/App.js
+++ src/App.js
@@ -2,37 +2,67 @@ import React from 'react';
 import './App.css';
 import { Component, useState } from 'react';
 
+
+  
+
+
 function App() {
 
-  const array_nombres = ['juanita', 'pepita', 'maria']
-  const[valueInput, setValueInput] = useState('') 
-  const[nombres,setNombres] = useState(array_nombres) 
-
-  function onChangeInput(e) {
-    setValueInput(e.target.value)
+  const auto = [
+    {
+      imagen:'https://cnnespanol.cnn.com/wp-content/uploads/2017/06/170628112455-jaguar-xe-sv-780x439.jpg?quality=100&strip=info',
+      marca: 'JAGUAR',
+      placa: 'ASD-456'
+      },
+    {
+      imagen:'https://cnnespanol.cnn.com/wp-content/uploads/2017/06/170628112455-jaguar-xe-sv-780x439.jpg?quality=100&strip=info',
+      marca: 'JAGUAR',
+      placa: 'ASD-456'
+      },
+    {
+      imagen:'https://cnnespanol.cnn.com/wp-content/uploads/2017/06/170628112455-jaguar-xe-sv-780x439.jpg?quality=100&strip=info',
+      marca: 'JAGUAR',
+      placa: 'ASD-456'
+    }
+  ]
+  
+  const [valorInput, setValorInput] = useState('')
+  const [array_carros,setArray_carros] = useState(auto)
+  
+  function agregarAnimal(e){
+    setValorInput(e.target.value)
   }
-
-  function onClickEnviar(){
-    const copyNombres = [...nombres]
-    copyNombres.push(valueInput)
-    setNombres(copyNombres)
-    setValueInput('')
+  function daleClick(){
+    if (valorInput === '') return
+    const copyArray_carros = [...array_carros]
+    copyArray_carros.push({
+      imagen:'https://cnnespanol.cnn.com/wp-content/uploads/2017/06/170628112455-jaguar-xe-sv-780x439.jpg?quality=100&strip=info',
+      marca: valorInput,
+      placa: 'ASD-456'
+    })
+    setArray_carros(copyArray_carros)
+    setValorInput('')
   }
-  return (
+  return(
     <div>
-      <div>
-        {nombres.map((e) => (<div>{e}</div>))}
+      <div> 
+        {array_carros.map((e,i)=> (
+          <div key={i}>
+            {e.marca} - {e.placa} - posición: {i}
+            {i === 1 ? '- SOY LA SEGUNDA POSICIÓN' : '' }
+            </div>
+        ))}
       </div>
-      <input 
-        value={valueInput} 
-        onChange={onChangeInput}/>
-      <button  
-        onClick={onClickEnviar}
-        >
-          Enviar
-      </button>
-    </div>
-  )
+      <div>
+        <input value={valorInput} onChange={agregarAnimal}/>
+            
+    
+        <button onClick={daleClick}>
+          Agregar
+        </button>
+      </div>
+    </div> 
+  ) 
 }
-
 export default App;
+
