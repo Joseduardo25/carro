9474e2cf2737d9a52e10229d369e71370c1f107f
Eduardo Calachua <jocdwardo25@gmail.com>
2020-08-03 19:49:43 -0500

    focos_2

--- src/App.js
+++ src/App.js
@@ -3,46 +3,46 @@ import './App.css';
 import { Component, useState } from 'react';
 
 function App() {
-
-  const persona = [
-    {
-      nombre: 'eduardo',
-      apellido: 'calachua',
-      edad: 25,
-      sexo: 'M',
-      direccion: 'calle - 123'
-    }
-  ]
-  const[valorInput, setValorInput] = useState('')
-  const[usuario, setUsuario] = useState(persona)
-
-  function newValue(e){
-    setValorInput(e.target.value)
+  const[luz1, setLuz1] = useState(true)
+  function changeFoco1(){
+    setLuz1(!luz1)
   }
-  function addValue(){
-    const copia_usuario = [...usuario]
-    copia_usuario.push({
-      nombre: 'eduardo',
-      apellido: 'calachua',
-      edad: 25,
-      sexo: 'M',
-      direccion: valorInput
-    })
-    setUsuario(copia_usuario)
-    setValorInput('')
+  const[luz2, setLuz2] = useState(true)
+  function changeFoco2(){
+    setLuz2(!luz2)
   }
-  
+  const[luz3, setLuz3] = useState(true)
+  function changeFoco3(){
+    setLuz3(!luz3)
+  }
   return(
     <div>
       <div>
-      {usuario.map((e) => (<div>{e.direccion}</div>))}
-      </div>
+        <div>
+          <img style={{width:'200px', heigth:'200px'}} src={luz1 ? 'https://live.staticflickr.com/5186/5854477079_bdb0e67ba2_b.jpg' : 'https://static8.depositphotos.com/1006187/899/i/950/depositphotos_8991916-stock-photo-bulb.jpg'}/>
+        </div>
+        <div>
+          <button onClick={changeFoco1}>{luz1 ? 'Apagar' : 'Encender'}</button>
+        </div>
+      </div> 
       <div>
-        <input value={valorInput} onChange={newValue}/>
-        <button onClick={addValue}>Agregar</button>
-      </div>
-    </div> 
-  ) 
+       <div>
+          <img style={{width:'200px', heigth:'200px'}} src={luz2 ? 'https://live.staticflickr.com/5186/5854477079_bdb0e67ba2_b.jpg' : 'https://static8.depositphotos.com/1006187/899/i/950/depositphotos_8991916-stock-photo-bulb.jpg'}/>
+        </div>
+       <div>
+          <button onClick={changeFoco2}>{luz2 ? 'Apagar' : 'Encender'}</button>
+       </div>
+      </div> 
+      <div>
+        <div>
+         <img style={{width:'200px', heigth:'200px'}} src={luz3 ? 'https://live.staticflickr.com/5186/5854477079_bdb0e67ba2_b.jpg' : 'https://static8.depositphotos.com/1006187/899/i/950/depositphotos_8991916-stock-photo-bulb.jpg'}/>
+        </div>
+        <div>
+          <button onClick={changeFoco3}>{luz3 ? 'Apagar' : 'Encender'}</button>
+         </div>
+      </div> 
+    </div>  
+ )   
 }
 export default App;
 
